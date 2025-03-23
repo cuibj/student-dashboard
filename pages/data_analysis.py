5c3f8f61ffbd8acc4b71d4ea97e293a30d8a95c3
cuibingjie <cuibingjie@amberweather.com>
2025-03-23 17:43:32 +0800

    更新一般

--- pages/data_analysis.py
+++ pages/data_analysis.py
@@ -3,13 +3,105 @@ import pandas as pd
 import openpyxl
 from sqlalchemy import create_engine
 import sqlite3
+import datetime
+import plotly.express as px
 
 # 获取数据
 @st.cache_data
-def get_base_info(refresh_date):
-    print(refresh_date)
+def get_data(date):
+    print(date)
+    db_path = '/Users/amber/workspace/student-dashboard/data/student.db'
+    conn = sqlite3.connect(db_path)
 
-    return product_base
+    data = pd.read_sql_query("SELECT * FROM scores", conn)
+    return data
+
+
+st.write("# 成绩分析")
+
+# if st.button("获取数据"):
+# today = datetime.date.today()
+now = datetime.datetime.now()
+data = get_data(now)
+st.write("## 班级分学科平均成绩")
+exam_lst = data["考试名称"].unique() 
+exam = st.selectbox("考试", exam_lst)
+
+cols1 = ['语文','数学','英语','物理','化学','生物','地理','政治','历史','总分']
+
+data0 = data.melt(id_vars=['班级','姓名', '考试名称',"考试日期"], value_vars = cols1,  var_name="学科", value_name="成绩")
+# st.dataframe(data0)
+data1 = data0.loc[data0["考试名称"]==exam, :].groupby(by = ["班级","学科"])["成绩"].mean().reset_index()
+# st.dataframe(data1)
+st.bar_chart(data1.loc[data1['学科']=="总分"], x="学科", y="成绩", color="班级", stack=False)
+st.bar_chart(data1.loc[data1['学科']!="总分"], x="学科", y="成绩", color="班级", stack=False)
+
+st.write("## 班级各科成绩分布")
+class_lst = data["班级"].unique()
+class_lst.sort()
+cl=st.selectbox("班级", class_lst, index=1)
+data2 = data0.loc[(data0["考试名称"]==exam)&(data0["班级"]==cl),:]
+
+f1,f2=st.columns((4, 10))
+fig0 = px.box(
+    data2.loc[data2["学科"]=="总分", :],
+    x="学科",          # 横坐标为学科
+    y="成绩",          # 纵坐标为成绩
+    title="各学科成绩分布箱线图",
+    color="学科",       # 按学科颜色区分（可选）
+    # points="all"       # 显示所有原始数据点
+)
+# 优化布局
+fig0.update_layout(
+    xaxis_title="学科",
+    yaxis_title="成绩",
+    hovermode="x unified",
+    showlegend=False,   # 隐藏图例（颜色已通过学科区分）
+    # hoverinfo="skip"  
+)
+f1.plotly_chart(fig0, use_container_width=True)
+
+# 绘制箱线图
+fig1 = px.box(
+    data2.loc[data2["学科"]!="总分", :],
+    x="学科",          # 横坐标为学科
+    y="成绩",          # 纵坐标为成绩
+    title="各学科成绩分布箱线图",
+    color="学科",       # 按学科颜色区分（可选）
+    # points="all"       # 显示所有原始数据点
+)
+# 优化布局
+fig1.update_layout(
+    xaxis_title="学科",
+    yaxis_title="成绩",
+    hovermode="x unified",
+    showlegend=False,   # 隐藏图例（颜色已通过学科区分）
+    # hoverinfo="skip"  
+)
+
+f2.plotly_chart(fig1, use_container_width=True)
+
+
+st.write("## 学生历史成绩分析")
+
+stu_lst = data.loc[data["班级"]==cl,"姓名"].unique()
+stu_lst.sort()
+
+stu = st.selectbox("学生", stu_lst)
+
+data0["排名"] = data0.groupby(by=["考试名称","考试日期","学科"])["成绩"].rank(method='min', ascending=False)
+
+stu_class = st.selectbox("学科", cols1)
+stu_data = data0.loc[(data0["学科"]==stu_class)&(data0["姓名"]==stu), : ]
+st.line_chart(stu_data, x="考试日期",y="排名")
+
+
+
+
+
+
+
+
 
 
 
--- pages/upload_data.py
+++ pages/upload_data.py
@@ -3,12 +3,70 @@ import pandas as pd
 import openpyxl
 from sqlalchemy import create_engine
 import sqlite3
+import datetime
+from st_btn_group import st_btn_group
+# from streamlit.components.v1 import html
 
 st.title('成绩分析')
 
 uploaded_file = st.file_uploader("Choose a file", type = 'xlsx')
 
+
+@st.cache_resource
+def get_conn():
+    db_path = '/Users/amber/workspace/student-dashboard/data/student.db'
+    conn = sqlite3.connect(db_path)
+    engine = create_engine(f'sqlite:///{db_path}', echo=False)
+    return conn,engine
+
+conn,engine = get_conn()
+
+
+def delete_insert(cur, data, exam_name):
+    del_sql = f'''
+        delete from scores 
+        where 考试名称 = '{exam_name}'
+    '''
+    cur = conn.cursor()
+    cur.execute(del_sql)
+    conn.commit()
+    data[['班级','姓名','总分','语文','数学','英语','物理','化学','生物','地理','政治','历史','考试名称','考试日期']].to_sql('scores', con=engine, if_exists='append', index=False)
+
+
+
+    
+
+
+
+
+@st.dialog("二次确认")
+def sbutton():
+    st.warning(f"该考试成绩数据已经存在，请确认是否覆盖")
+    ans = st_btn_group(
+        buttons=[{"label": "确认", "value": "1", "style": {"color": "red", "font-size": "20px"}},
+                  {"label": "取消", "value": "2"}],   
+    )  
+    if ans == '2':
+        st.rerun()
+    if ans == '1':
+        st.session_state.upload = True
+        st.rerun()
+        
+    # st1, st2 = st.columns(2)
+    # if st1.button("确认"):
+    #     return True
+    # if st2.button("取消"): 
+    #     return False
+        # del_sql = f'''delete from scores where 考试名称 = '{exam_name}' '''
+        
+        # engine = create_engine(f'sqlite:///{db_path}', echo=False)
+        # data.to_sql('scores', con=engine, if_exists='append', index=False)
+        # st.success("Done!")
+
+
+
 if uploaded_file is not None:
+    cur =conn.cursor()
     wb = openpyxl.load_workbook(uploaded_file)
     sheetnames = wb.sheetnames
     sheet_selector = st.selectbox("Select sheet:",sheetnames)
@@ -16,18 +74,31 @@ if uploaded_file is not None:
     st.markdown(f"### Currently Selected: `{sheet_selector}`")
     st.write(df)
     exam_name = st.text_input("考试名称")
-    df["考试"] = exam_name
+    df["考试名称"] = exam_name
+    date = st.date_input("考试日期", datetime.date.today())
+    date_str = date.strftime('%Y-%m-%d')
+    df["考试日期"] = date_str
     bt = st.button("上传数据")
     if bt:
-        db_path = '/Users/amber/workspace/stu_dashboard/data/student.db'  # 替换为你的路径
-        engine = create_engine(f'sqlite:///{db_path}', echo=False)
-        df.to_sql('scores', con=engine, if_exists='append', index=False)
-        st.success("Done!")
+        
+        # engine = create_engine(f'sqlite:///{db_path}', echo=False)
 
-
-
-    
-    
-
-
+        # df.to_sql('scores', con=engine, if_exists='append', index=False)
+        # st.success("Done!")
+        
+        # 检查考试名称，并判断是否上传
+        test_sql = f'''select count(*) from scores where 考试名称 = '{exam_name}' '''
+        # cur = conn.cursor()
+        cur.execute(test_sql)
+        ans = cur.fetchone()
+        
+        if ans[0] > 0:
+            # st.warning("该考试已经存在，请确认是否覆盖")
+            sbutton()
+        else: 
+            st.session_state.upload = True
+    if "upload" in st.session_state and st.session_state.upload:
+        delete_insert(cur,df, exam_name)
+        st.write("upload Scucess")
+        del st.session_state["upload"]
 
